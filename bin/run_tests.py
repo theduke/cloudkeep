7d852314cfef87fd5956d0c5f913c19b81a98d87
theduke <chris@theduke.at>
2013-01-07 21:08:29 +0100

    better test runner

--- bin/run_tests.py
+++ bin/run_tests.py
@@ -1,6 +1,7 @@
 #! /bin/env python
 
 import sys, os
+import yaml
 
 # set up PYTHONPATH
 path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -13,9 +14,22 @@ from mcb.runner import Runner
 from mcb.frontends.cli import getCliRunner
 
 config = mcb.config.Config()
-config.fromFile(path + '/tests/conf1.yaml', 'yaml')
+data = yaml.load(open(path + '/tests/conf1.yaml'))
+config.outputs = data['outputs']
 
-runner = getCliRunner(config)
-runner.run()
-runner.saveConfig()
+SERVICES_TO_TEST=[
+  #'mcb.services.github.GithubService',
+  #'mcb.services.dropbo.DropboxService'
+  #'mcb.services.email.EmailImapService',
+  #'mcb.services.google.CalendarService',
+  'mcb.services.google.GmailService'
+]
+
+for name, conf in data['services'].items():
+  if name in SERVICES_TO_TEST:
+    config.services = {name: conf}
+
+    runner = getCliRunner(config)
+    runner.run()
+    #runner.saveConfig()
 
