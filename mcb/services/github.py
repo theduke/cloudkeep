2415bf807a3be1726dca8b5e8f0a18585a90c4c7
theduke <chris@theduke.at>
2013-01-12 14:57:46 +0100

    Github service now can back up issues

--- mcb/services/github.py
+++ mcb/services/github.py
@@ -21,23 +21,27 @@ class GithubService(Service):
     self.addConfig('password', default='')
 
     self.addConfig('user', default='')
-    self.addConfig('mirror_repos', 'bool', default=True)
 
+    self.addConfig('mirror_repos', 'bool', default=True)
     self.addConfig('compress_repos', default=False, description="""
 Compress the repository into a tar archive, with optional bzip2/gzip compression.
 Compression is required for non FileSystem outputs.
 Options: [none, tar, gz, bz2]
 """)
 
+    self.addConfig('issues', 'bool', default=True)
+
   def getPluginOutputPrefix(self):
     return self.username
 
-  def getRepos(self):
-    url = self.apiUrl + '/users/' + self.username + '/repos'
+  def doRequest(self, path, decode=True):
+    url = self.apiUrl + path
     r = requests.get(url, auth=(self.username, self.password))
-    repos = json.loads(r.text)
 
-    return repos
+    return json.loads(r.text) if decode else r.text
+
+  def getRepos(self):
+    return self.doRequest('/users/' + self.username + '/repos')
 
   def checkForGit(self):
     if call(['git', '-h'])[0] != 0:
@@ -49,33 +53,45 @@ Options: [none, tar, gz, bz2]
     Note that only the FileSystem output supports non-compressed mode.
     """
 
-    if self.compress_repos:
-      # glone the git repo
+    if not self.compress_repos:
+      raise Exception("Non-compressed mode not implemented yet")
 
+    # clone the git repo
 
-      path = self.tmpPath + os.path.sep + name + '.git'
+    path = self.tmpPath + os.path.sep + name + '.git'
 
-      self.logger.debug('Cloning git repo {url} to {path}'.format(
-        url=url, path=path
+    self.logger.debug('Cloning git repo {url} to {path}'.format(
+      url=url, path=path
+    ))
+    result = call(['git', 'clone', '--mirror', url, path])
+
+    if result[0] != 0:
+      raise Exception('Could not clone {repo}: {o}'.format(
+        repo=url, o=result[1] + "\n\n" + result[2]
       ))
-      result = call(['git', 'clone', '--mirror', url, path])
 
-      if result[0] != 0:
-        raise Exception('Could not clone {repo}: {o}'.format(
-          repo=url, o=result[1] + "\n\n" + result[2]
-        ))
+    # create archive and write it
+    self.logger.debug('Compressing repo into archive...')
 
-      # create archive and write it
-      self.logger.debug('Compressing repo into archive...')
+    fileName = name + '.git.tar'
 
-      fileName = name + '.git.tar'
-      if self.compress_repos != 'tar': fileName += '.' + self.compress_repos
-      compression = self.compress_repos if self.compress_repos != 'tar' else None
-      fileObject = self.output.getStream(fileName, name, 'w')
-      createArchive(path, compression, fileObject)
+    if self.compress_repos != 'tar': fileName += '.' + self.compress_repos
+    compression = self.compress_repos if self.compress_repos != 'tar' else None
+
+    fileObject = self.output.getStream(fileName, name, 'w')
+    createArchive(path, compression, fileObject)
+
+  def saveIssues(self, name):
+    path = '/repos/theduke/' + name + '/issues'
+    data = self.doRequest(path, decode=False)
+
+    self.output.set('issues.json', data, bucket=name)
 
   def runBackup(self):
     self.tmpPath = self.getTmpPath()
     for repo in self.getRepos():
       if self.mirror_repos:
         self.mirrorRepo(repo['name'], repo['git_url'])
+
+        if repo['has_issues']:
+          self.saveIssues(repo['name'])
